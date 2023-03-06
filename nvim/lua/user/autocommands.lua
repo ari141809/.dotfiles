49b1878aa2c72ce79290b95d47d19ba497103b59
ari <115173089+ari141809@users.noreply.github.com>
2023-03-05 23:59:40 -0800

    Delete nvim directory

--- nvim/lua/user/autocommands.lua
+++
@@ -1,6 +0,0 @@
-vim.cmd [[
-  autocmd TermOpen * startinsert
-  autocmd filetype cpp nnoremap <F9> :w <bar> !g++ -std=c++14 % -o %:r -Wl,--stack,268435456<CR>
-  autocmd filetype cpp nnoremap <F10> :TermExec cmd="%:r"<CR>
-  autocmd filetype cpp nnoremap <C-C> :s/^\(\s*\)/\1\/\/<CR> :s/^\(\s*\)\/\/\/\//\1<CR> $
-]]
